e0c7751f7cc039031845a70cf43833e8d80bd8f2
Roger Tan <rtan4955@uni.sydney.edu.au>
2023-07-11 21:51:56 +1000

    changed alt

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -12,6 +12,7 @@ export default function Thumbnail({ movie }: Props) {
   return (
     <div className="relative h-28 min-w-[180px] curosr-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105">
       <Image
+      alt = "main movie image"
         src={`https://image.tmdb.org/t/p/w500${
           movie.backdrop_path || movie.poster_path
         }`}
--- pages/Banner.tsx
+++ pages/Banner.tsx
@@ -18,7 +18,7 @@ function Banner({ netflixOriginals }: Props) {
   return (
     <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
       <div className="absolute top-0 left-0 -z-10 h-[95vh] w-screen">
-        <Image
+        <Image alt="movie_cover"
           src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
           layout="fill"
           objectFit="cover"
