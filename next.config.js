3d9e7acd77c5830877170b70fd23f87aa929316d
Roger Tan <rtan4955@uni.sydney.edu.au>
2023-07-17 22:27:59 +1000

    debug

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const withTM = require("next-transpile-modules")([
-  // "@stripe/firestore-stripe-payments",
+  "@stripe/firestore-stripe-payments",
 ]); // pass the modules you would like to see transpiled
 
 module.exports = withTM({
